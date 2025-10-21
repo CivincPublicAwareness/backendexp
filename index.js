5750bd77333106819fdaed01fd9b29032c5a6cb0
iamthehr <harshraj5565@gmail.com>
2025-10-21 15:01:42 +0530

    added ci/cd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const analyticsRoutes = require("./routes/analyticsRoutes");
 const feedbackRoutes = require("./routes/feedbackRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Load configuration files
 try {
